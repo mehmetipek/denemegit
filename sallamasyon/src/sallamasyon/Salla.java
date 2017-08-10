f2211e015eac8f9ef6faaead8886d720e0e9d7e2
szrozr <sezerozer@yandex.com>
2017-08-10 19:35:26 +0200

    asdasd

--- sallamasyon/src/sallamasyon/Salla.java
+++ sallamasyon/src/sallamasyon/Salla.java
@@ -5,7 +5,7 @@ public class Salla {
 	public static void main(String[] args) {
 		System.out.println("deneme123");
 		System.out.println("sasdasd");
-		//deneme 123123123123asdasd
+		//deneme 123123123123asdasdasdasd
 		
 	}
 
