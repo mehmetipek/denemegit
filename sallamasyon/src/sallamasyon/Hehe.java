923bea08b4346a5d6b2c7c32c19086292b1d4cb3
szrozr <sezerozer@yandex.com>
2017-08-11 15:01:32 +0200

    eski hehe çıkarıldı yeni Hehe eklendi

---
+++ sallamasyon/src/sallamasyon/Hehe.java
@@ -0,0 +1,10 @@
+package sallamasyon;
+
+public class Hehe {
+
+	public static void main(String[] args) {
+		System.out.println("yeni hehe");
+
+	}
+
+}
--- sallamasyon/src/sallamasyon/hehe.java
+++
@@ -1,5 +0,0 @@
-package sallamasyon;
-
-public class hehe {
-
-}
